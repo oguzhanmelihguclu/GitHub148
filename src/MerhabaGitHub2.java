b895a7992f92136341f87545e4ea0a49bd858a66
oguzhanmelihguclu <oguzhanmguclu@gmail.com>
2024-11-16 15:28:06 +0300

    commit mesajı

--- src/MerhabaGitHub2.java
+++ src/MerhabaGitHub2.java
@@ -2,5 +2,8 @@ public class MerhabaGitHub2 {
     public static void main(String[] args) {
         System.out.println("Evet bu 2.gönderim olacak");
         System.out.println("bugun nasılsın?");
+        int sayi1=10;
+        int sayi=15;
+        System.out.println(sayi+sayi1);
     }
 }
